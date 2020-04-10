c6289be84cfea1d1593731223f4003fc73ee476f
Roberto Salim <salimroberto95@gmail.com>
2020-04-11 05:41:48 +0800

    done bitcoin-ticker

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,26 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future getCoinData(String currency) async {
+    Map<String, String> cryptoPrices = {};
+
+    for (String crypto in cryptoList) {
+      http.Response response = await http.get(
+        'https://apiv2.bitcoinaverage.com/indices/global/ticker/$crypto$currency',
+        headers: {'x-ba-key': "NTE2N2I5Njc3ZTc5NGY2NGFlNDE3YzdhZWU2ZWVlZGI"},
+      );
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['last'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw ('Problem with the get request');
+      }
+    }
+
+    return cryptoPrices;
+  }
+}
